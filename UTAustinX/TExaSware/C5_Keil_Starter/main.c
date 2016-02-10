997790d9ee6cd30b95f578f8e27247757440a5de
glennlopez <glennlopez@gmail.com>
2016-02-10 06:24:45 -0600

    finished lesson 5.5

--- UTAustinX/TExaSware/C5_Keil_Starter/main.c
+++ UTAustinX/TExaSware/C5_Keil_Starter/main.c
@@ -9,15 +9,52 @@
 #include <stdio.h>  // Diamond braces for sys lib: Standard I/O
 #include "uart.h"   // Quotes for user lib: UART lib
 
+//global vars
+unsigned long error;
+
+
+//Function prototypes
+void init_global();
+unsigned long calc_area(unsigned long);
+
 // Subroutines section
 // MAIN: Mandatory subroutine for a C Program to be executable
  int main(void) {
-	 unsigned long side;	//size of the wall in meters
-	 unsigned long area; //size of the room in m^2
-	UART_Init();   // call subroutine to init uart lib
-	side = 3;				//3 meters
-	area = side * side; //in square meters 
-	//printf("This program calculates areas of square shaped rooms");
-	 printf("This room with %ld by %ld is %ld meters squared \n", side, side, area);
+	  
+	unsigned long side;					//size of the wall in meters
+	unsigned long area; 				//size of the room in m^2
+	 
+	UART_Init();   							// call subroutine to init uart lib
+	init_global();							// call the default global subroutine
+	
+	side = 1;										//in meters
+	area = calc_area(side); 		//in square meters 
+	printf("\nThis room with %ld by %ld is %ld meters squared \n", side, side, area);
+	 
+	while(1){
+		if(side >=40){
+			break;
+		}
+		side = side + 1;
+		area = calc_area(side); 		//in square meters 
+		printf("\nThis room with %ld by %ld is %ld meters squared \n", side, side, area);
+	}
+	
+}
+ 
+void init_global(){
+	error = 0;
+}
+ 
+unsigned long calc_area(unsigned long s){
+	unsigned long result;
+	if(s <= 25){
+		result = s * s;
+	}
+	else{
+		result = 0;					//mistake
+		error = error + 1;
+	}
+	return result;
 }
  
