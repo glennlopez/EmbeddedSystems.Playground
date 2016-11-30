70a1049b6d11f7b00e0766a6f6f4ce74fedd0598
Glenn Lopez <glennlopez@gmail.com>
2016-11-29 20:37:32 -0600

    added pre-lab comments

--- UTAustinX/Labware/Lab7_HeartBlock/main.c
+++ UTAustinX/Labware/Lab7_HeartBlock/main.c
@@ -24,24 +24,24 @@
 #include "TExaS.h"
 
 // Constant declarations to access port registers using 
-// symbolic names instead of addresses
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+// PortF Base Address: 0x4002.5FFF
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC)) //offset: 0x3FC 	- enabled pins for data I/O (4*2^b)
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400)) //offset: 0x400		- 1 is output, 0 is input
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420)) //offset:	0x420		- 1 enabled, 0 disabled
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510)) //offset:	0x510		- 1 pullup enabled, 0 disabled
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C)) //offset:	0x51C		- 1 enabled, 0 disabled
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520)) //offset:	0x520		- 0x4C4F434B unlocks GPIO commit register
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524)) //offset:	0x524		- 1 writeable, 0 cannot be written
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528)) //offset:	0x528		- 1 enabled, 0 disabled
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C)) //offset:	0x52C		- 0x00000000 resets 
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108)) //offset:	0x108		- 
 // 2. Declarations Section
 //   Global Variables
 
 //   Function Prototypes
 void PortF_Init(void);
 void Delay1ms(unsigned long msec);
-void EnableInterrupts(void);  			// Enable interrupts
+void EnableInterrupts(void);
 void WaitForASLow(void);
 void WaitForASHigh(void);
 void SetVT(void);
@@ -71,6 +71,9 @@ int main(void){
     // i) VT signal goes low
 		
 		
+		
+		
+		
   }
 }
 
@@ -85,17 +88,31 @@ int main(void){
  * Notes: ...
  */
 void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R 							|= 0x00000020;      		// 1) F clock
-  delay 											 = SYSCTL_RCGC2_R;      // delay to allow clock to stabilize     
-  GPIO_PORTF_AMSEL_R 					&= 0x00;        				// 2) disable analog function
-  GPIO_PORTF_PCTL_R 					&= 0x00000000;   				// 3) GPIO clear bit PCTL  
-  GPIO_PORTF_DIR_R 						&= ~0x10;         			// 4.1) PF4 input,
-  GPIO_PORTF_DIR_R 						|= 0x0E;          			// 4.2) PF3,2,1 output  
-  GPIO_PORTF_AFSEL_R 					&= 0x00;        				// 5) no alternate function
-  GPIO_PORTF_PUR_R 						|= 0x10;          			// 6) enable pullup resistor on PF4       
-  GPIO_PORTF_DEN_R 						|= 0x1E;          			// 7) enable digital pins PF4-PF1
+  SYSCTL_RCGC2_R 							|= 		0x00000020;      			// 1) F clock
+  delay 											 = 		SYSCTL_RCGC2_R;      	// delay to allow clock to stabilize     
+  GPIO_PORTF_AMSEL_R 					&= 		0x00;        					// 2) disable analog function
+  GPIO_PORTF_PCTL_R 					&= 		0x00000000;   				// 3) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R 						&= 		~0x10;         				// 4.1) PF4 input,
+  GPIO_PORTF_DIR_R 						|= 		0x0E;          				// 4.2) PF3,2,1 output  
+  GPIO_PORTF_AFSEL_R 					&= 		0x00;        					// 5) no alternate function
+  GPIO_PORTF_PUR_R 						|= 		0x10;          				// 6) enable pullup resistor on PF4       
+  GPIO_PORTF_DEN_R 						|= 		0x1E;          				// 7) enable digital pins PF4-PF1
 }
 
+/*
+		// Configure PortF pins
+		SYSCTL_RCGC2_R					|=	0x00000020;				// _ _ F E  D C B A (Port selection)
+		delay 									= 	SYSCTL_RCGC2_R;
+	
+		GPIO_PORTF_LOCK_R				=		0x4C4F434B;				// _ _ _ 1  1 1 1 1	(enable write access to GPIOCR register) 
+		GPIO_PORTF_CR_R					=		0x1F;							// _ _ _ 1  1 1 1 1	(Change Commit registers)
+		GPIO_PORTF_AMSEL_R			=		0x00;							// _ _ _ 0  0 0 0 0	(Not using analog mode select)
+		GPIO_PORTF_PCTL_R				=		0x00000000;				// _ _ _ 0  0 0 0 0 (Not using alternative functions)
+		GPIO_PORTF_DIR_R 				= 	0x0E;							// _ _ _ 0  1 1 1 0 (PF0 and PF4 are input switches)
+		GPIO_PORTF_AFSEL_R			=		0x00;							// _ _ _ 0  0 0 0 0 (Not using alternative functions)
+		GPIO_PORTF_PUR_R				=		0x11;							// _ _ _ 1  0 0 0 1 (Enable pull-up resistors on P0 and P4 switches)
+		GPIO_PORTF_DEN_R				=		0x1F; 						// _ _ _ 1  1 1 1 1	(Enable digital signal for all target pins)
+*/
 
 
 
@@ -189,7 +206,7 @@ void ClearReady(void){
  * Notes:   assumes 80 MHz clock 
  */
 void Delay1ms(unsigned long msec){
-// write this function
+	
 
 }
 
