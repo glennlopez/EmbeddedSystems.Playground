3092eda0ae46a808539cd44797c9b8d66c6ca337
Glenn Lopez <glennlopez@gmail.com>
2016-12-07 01:35:49 -0600

    added debug PortA
    this will be used for debbuging timers

--- Lessons/UTAustinX/Labware/Lab7_HeartBlock/main.c
+++ Lessons/UTAustinX/Labware/Lab7_HeartBlock/main.c
@@ -23,7 +23,11 @@
 // 1. Pre-processor Directives Section
 #include "TExaS.h"
 
+
 // Constant declarations to access port registers using 
+// Common port registers 
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108)) //offset:	0x108		- *Register 136: Clock gating control register
+	
 // PortF Base Address: 0x4002.5FFF
 #define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC)) //offset: 0x3FC 	- enabled pins for data I/O (4*2^b)
 #define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400)) //offset: 0x400		- 1 is output, 0 is input
@@ -34,10 +38,48 @@
 #define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524)) //offset:	0x524		- 1 writeable, 0 cannot be written
 #define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528)) //offset:	0x528		- 1 enabled, 0 disabled
 #define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C)) //offset:	0x52C		- 0x00000000 resets 
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108)) //offset:	0x108		-  
+
+
+	/* D E B U G   P O R T   P A R A M E T E R S
+	* PA3: 		Output 	(GREEN LED) - Ready signal
+	* PA2: 		Output 	(RED LED)		- Venticular trigger
+	* PA5:		Output	( ??? )			-	Unknown
+	* PA4: 		Input 	(SWITCH)		- Atrial sensor
+	*/
+
+
+// PortA Base Address (APB): 0x4000.4000 (for debug)
+#define GPIO_PORTA_DATA_R       (*((volatile unsigned long *)0x400043FC)) //offset: 0x3FC 	- enabled pins for data I/O (4*2^b)
+#define GPIO_PORTA_DIR_R        (*((volatile unsigned long *)0x40004400)) //offset: 0x400		- 1 is output, 0 is input
+#define GPIO_PORTA_AFSEL_R      (*((volatile unsigned long *)0x40004420)) //offset:	0x420		- 1 enabled, 0 disabled
+#define GPIO_PORTA_PUR_R        (*((volatile unsigned long *)0x40004510)) //offset:	0x510		- 1 pullup enabled, 0 disabled
+#define GPIO_PORTA_DEN_R        (*((volatile unsigned long *)0x4000451C)) //offset:	0x51C		- 1 enabled, 0 disabled
+#define GPIO_PORTA_LOCK_R       (*((volatile unsigned long *)0x40004520)) //offset:	0x520		- 0x4C4F434B unlocks GPIO commit register
+#define GPIO_PORTA_CR_R         (*((volatile unsigned long *)0x40004524)) //offset:	0x524		- 1 writeable, 0 cannot be written
+#define GPIO_PORTA_AMSEL_R      (*((volatile unsigned long *)0x40004528)) //offset:	0x528		- 1 enabled, 0 disabled
+#define GPIO_PORTA_PCTL_R       (*((volatile unsigned long *)0x4000452C)) //offset:	0x52C		- 0x00000000 resets 
+
+
+
+// PortF Bit-specific Address definitions (7|200, 6|100, 5|80, 4|40, 3|20, 2|10, 1|08, 0|04) expressed as 4*2^b (bitspecific addressing)
+#define LED_BLUE								(*((volatile unsigned long *)0x40025010))			//PF2 - offset 0x010 
+#define LED_RED									(*((volatile unsigned long *)0x40025008))			//PF1 - offset 0x008 
+#define LED_GREEN								(*((volatile unsigned long *)0x40025020))			//PF3 - offset 0x020 
+#define SW1											(*((volatile unsigned long *)0x40025040))			//PF4 - offset 0x040 
+#define SW2											(*((volatile unsigned long *)0x40025004))			//PF0 - offset 0x004
 	
-// Debug port for timing methods
-// PortA Base Address (APB): 0x4000.4000
+	
+// PortA Bit-specific Address definitions (7|200, 6|100, 5|80, 4|40, 3|20, 2|10, 1|08, 0|04) expressed as 4*2^b (bitspecific addressing)
+#define PA2											(*((volatile unsigned long *)0x40004010))			//OUTPUT	VT
+#define PA3											(*((volatile unsigned long *)0x40004020))			//OUTPUT	READY
+#define PA4											(*((volatile unsigned long *)0x40004040))			//INPUT		AS
+#define PA5											(*((volatile unsigned long *)0x40004080))			//OUTPUT	??
+
+
+
+// Warning: Only use these for bitspecific addresses
+#define OFF 										0x00	//not an address, no need to typecast to volatile unsigned long
+#define ON											0xFF	//not an address, no need to typecast to volatile unsigned long
 
 
 
@@ -47,6 +89,7 @@
 
 //   Function Prototypes
 void PortF_Init(void);
+void PortA_Init(void);
 void Delay1ms(unsigned long msec);
 void EnableInterrupts(void);
 void WaitForASLow(void);
@@ -60,7 +103,8 @@ void ClearReady(void);
 // MAIN: Mandatory for a C Program to be executable
 int main(void){
   TExaS_Init(SW_PIN_PF40, LED_PIN_PF31,ScopeOn);  		// activate grader and set system clock to 80 MHz
-  PortF_Init();                            						// Init port PF4 PF3 PF1    
+  PortF_Init();                            						// Init port F    
+	PortA_Init();																				// Init port A
   EnableInterrupts();                      						// enable interrupts for the grader  
 	
 	
@@ -78,7 +122,8 @@ int main(void){
     // i) VT signal goes low
 		
 		
-		
+		LED_GREEN = ON;
+		PA3 = ON;
 		
 		
   }
@@ -95,7 +140,7 @@ int main(void){
  * Notes: ...
  */
 void PortF_Init(void){ volatile unsigned long delay;
-  SYSCTL_RCGC2_R 							|= 		0x00000020;      			// 1) F clock
+  SYSCTL_RCGC2_R 							|= 		0x00000020;      			// 1) F clock gate enbale
   delay 											 = 		SYSCTL_RCGC2_R;      	// delay to allow clock to stabilize     
   GPIO_PORTF_AMSEL_R 					&= 		0x00;        					// 2) disable analog function
   GPIO_PORTF_PCTL_R 					&= 		0x00000000;   				// 3) GPIO clear bit PCTL  
@@ -106,20 +151,20 @@ void PortF_Init(void){ volatile unsigned long delay;
   GPIO_PORTF_DEN_R 						|= 		0x1E;          				// 7) enable digital pins PF4-PF1
 }
 
-/*
-		// Configure PortF pins
-		SYSCTL_RCGC2_R					|=	0x00000020;				// _ _ F E  D C B A (Port selection)
-		delay 									= 	SYSCTL_RCGC2_R;
+void PortA_Init(void){ volatile unsigned long delay;
+	SYSCTL_RCGC2_R 							|= 		0x00000001;      			// 1) A clock gate enable
+  delay 											 = 		SYSCTL_RCGC2_R;      	// delay to allow clock to stabilize  
+	GPIO_PORTA_LOCK_R						 =		0x4C4F434B;						// Check 0x4C4F.434B in datasheet
+	GPIO_PORTA_CR_R					 		 =		0x2F;									// _ _ 1 1  1 1 1 1	(Change Commit registers)
+  GPIO_PORTA_AMSEL_R 					&= 		0x00;        					// 2) disable analog function
+  GPIO_PORTA_PCTL_R 					&= 		0x00000000;   				// 3) GPIO clear bit PCTL  
+  GPIO_PORTA_DIR_R 						&= 		~0x10;         				// 4.1) PA4 input
+  GPIO_PORTA_DIR_R 						|= 		0x2C;          				// 4.2) PA3, PA2, PA5 output  
+  GPIO_PORTA_AFSEL_R 					&= 		0x00;        					// 5) no alternate function
+  GPIO_PORTA_PUR_R 						|= 		0x10;          				// 6) enable pullup resistor on PA4       
+  GPIO_PORTA_DEN_R 						|= 		0x3C;          				// 7) enable digital pins PA5 PA4 PA3 PA2
 	
-		GPIO_PORTF_LOCK_R				=		0x4C4F434B;				// _ _ _ 1  1 1 1 1	(enable write access to GPIOCR register) 
-		GPIO_PORTF_CR_R					=		0x1F;							// _ _ _ 1  1 1 1 1	(Change Commit registers)
-		GPIO_PORTF_AMSEL_R			=		0x00;							// _ _ _ 0  0 0 0 0	(Not using analog mode select)
-		GPIO_PORTF_PCTL_R				=		0x00000000;				// _ _ _ 0  0 0 0 0 (Not using alternative functions)
-		GPIO_PORTF_DIR_R 				= 	0x0E;							// _ _ _ 0  1 1 1 0 (PF0 and PF4 are input switches)
-		GPIO_PORTF_AFSEL_R			=		0x00;							// _ _ _ 0  0 0 0 0 (Not using alternative functions)
-		GPIO_PORTF_PUR_R				=		0x11;							// _ _ _ 1  0 0 0 1 (Enable pull-up resistors on P0 and P4 switches)
-		GPIO_PORTF_DEN_R				=		0x1F; 						// _ _ _ 1  1 1 1 1	(Enable digital signal for all target pins)
-*/
+}
 
 
 
