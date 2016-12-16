f69d60f66afe5bff1906745b6f91a2ea935aa849
Glenn Lopez <glennlopez@gmail.com>
2016-12-16 15:39:00 -0600

    update

--- Finished Labs/Lab6/README.md
+++ Finished Labs/Lab6/README.md
@@ -1,1 +1,19 @@
 [![IMAGE ALT TEXT](http://img.youtube.com/vi/hQVVe-152t4/0.jpg)](http://www.youtube.com/watch?v=hQVVe-152t4 "Video Title")
+
+STARTER PROJECT
+
+Lab6_BranchingFunctionsDelays
+
+PURPOSE
+=================
+The purpose of this lab is to learn simple programming structures in C. You will also learn how to estimate how long it takes to run software, and use this estimation to create a time delay function. You learn how to use the oscilloscope to measure time delay. C software skills you will learn include masking, toggling, if-then, subroutines, and looping.
+
+SYSTEM REQUIREMENTS
+=================
+The system has one input switch and one output LED. Figure 6.1 shows the system when simulated as the switch is touched. A negative logic switch means the PF4 signal will be 1 (high, 3.3V) if the switch is not pressed, and the PF4 signal will be 0 (low, +0V) if the switch is pressed. A positive logic blue LED interface means if the software outputs a 1 to PF2 (high, +3.3V) the LED will turn ON, and if the software outputs a 0 to PF2 (low, 0V) the blue LED will be OFF. Here in Lab 6, you first debug in simulation and then run on the real board, but no external components on the protoboard will be required. The switch and LED are already built into the LaunchPad. However in Lab 8, you will attach a real switch and a real LED to your protoboard (solderless breadboard) and interface them to your microcontroller. Overall functionality of this system is described in the following rules.
+</br>
+1) Make PF2 an output and make PF4 an input (enable PUR for PF4). 
+2) The system starts with the LED ON (make PF2 =1). 
+3) Delay for about 100 ms
+4) If the switch is pressed (PF4 is 0), then toggle the LED once, else turn the LED ON. 
+5) Repeat steps 3 and 4 over and over.
--- Lessons/UTAustinX/Labware/Lab8_SwitchLEDinterface/SwitchLEDInterface.c
+++ Lessons/UTAustinX/Labware/Lab8_SwitchLEDinterface/SwitchLEDInterface.c
@@ -1,11 +1,15 @@
+// 0.Documentation Section 
+// Lab8_SwitchLEDInterface, SwitchLEDInterface.c, glennlopez
+
+
+// 1. Pre-processor Directives Section
 // Includes
 #include "TExaS.h"
 
 // Clock gating controls base address: 0x400F.E000
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))			//run mode gating register		- offset 0x108 
 
-	
-// PortE(APB) base address: 0x4002.4000
+// PortE (APB) base address: 0x4002.4000
 #define GPIO_PORTE_DATA_R       (*((volatile unsigned long *)0x40024000))			//0x3FC means 0011 1111 expressed as 4*2^b (bitspecific addressing)
 #define GPIO_PORTE_DIR_R        (*((volatile unsigned long *)0x40024400))			//gpio direction 							- offset 0x400
 #define GPIO_PORTE_AFSEL_R      (*((volatile unsigned long *)0x40024420))			//gpio alternte funtion 			- offset 0x420
@@ -13,46 +17,46 @@
 #define GPIO_PORTE_DEN_R        (*((volatile unsigned long *)0x4002451C))			//gpio digital enable 				- offset 0x51C
 #define GPIO_PORTE_AMSEL_R      (*((volatile unsigned long *)0x40024528))			//gpio analog mode select			- offset 0x528
 #define GPIO_PORTE_PCTL_R       (*((volatile unsigned long *)0x4002452C))			//gpio port control						- offset 0x52C
-#define GPIO_PORTE_LOCK_R				(*((volatile unsigned long *)0x40024520))			//gpio portf lock register 		- offset 0x520
-#define GPIO_PORTE_CR_R					(*((volatile unsigned long *)0x40024524))			//gpio portf comit register		- offset 0x524
 	
 // PortE Bit-specific Address definitions (7|200, 6|100, 5|80, 4|40, 3|20, 2|10, 1|08, 0|04) expressed as 4*2^b (bitspecific addressing)
 #define SW1											(*((volatile unsigned long *)0x40024004))			//OUT - offset
 #define LED											(*((volatile unsigned long *)0x40024008))			//OUT - offset
 
-// Warning: Only use these for bitspecific addresses
+// Warning: Only use these for bitspecific addresses (do not use for inputs)
 #define OFF 										0x00	//not an address, no need to typecast to volatile unsigned long
 #define ON											0xFF	//not an address, no need to typecast to volatile unsigned long
 
 
+// 2. Declarations Section
+// Global Variables & prototypes
+void DisableInterrupts(void); 			
+void EnableInterrupts(void);  			
+void initPortE(void);								
+void delay1ms(unsigned long msec);	
+void blink(int ms);									
 
-// Basic functions defined at end of startup.s
-void DisableInterrupts(void); 				// Disable interrupts
-void EnableInterrupts(void);  				// Enable interrupts
-int initPortE(void);									// PortA prototype
-void blink(int ms);										// Blink LED in miliseconds
-void delay1ms(unsigned long msec);		// Delay subroutine req for blink
 
-
-
+// 3. Subroutines Section
 int main(void){ 
 	
-	TExaS_Init(SW_PIN_PE0, LED_PIN_PE1, ScopeOff);  
+	TExaS_Init(SW_PIN_PE0, LED_PIN_PE1, ScopeOn);  
   EnableInterrupts();         // enable interrupts for the grader
-	initPortE();								// configure portA - for debugging
+	initPortE();								// initialize port E
 	
+	// keep LED on
   while(1){										
-
-		if(SW1 == OFF){
+		if(SW1 == 0x00){
 			LED = ON;
 		}
+		
+		//blink if switch is not 0x00 (off)
 		else{
 			delay1ms(10);						//debounce
 			if(SW1 == 0x01){				//check if switch still on
 				blink(100);						//blink the led
 			}
 		}
-
+		
 	}
 }
 
@@ -61,6 +65,11 @@ int main(void){
 
 
 
+/* _ Subroutine
+ * Inputs: None
+ * Outputs: None
+ * Notes: 
+ */
 void delay1ms(unsigned long msec){
 	int i = 0;
 	while(msec > 0){
@@ -69,7 +78,11 @@ void delay1ms(unsigned long msec){
 	}
 }
 
-
+/* _ Subroutine
+ * Inputs: None
+ * Outputs: None
+ * Notes: 
+ */
 void blink(int ms){
 			LED = ON;
 			delay1ms(ms);
@@ -77,21 +90,22 @@ void blink(int ms){
 			delay1ms(ms);
 }
 
-
-int initPortE(void){unsigned long volatile delay;
-
-		SYSCTL_RCGC2_R					|=	0x00000010;				// _ _ F E  D C B A (Port selection)
-		delay 									= 	SYSCTL_RCGC2_R;
+/* _ Subroutine
+ * Inputs: None
+ * Outputs: None
+ * Notes: 
+ */
+void initPortE(void){unsigned long volatile delay;
 	
-		GPIO_PORTE_LOCK_R				=		0x4C4F434B;				// _ _ _ 1  1 1 1 1	(enable write access to GPIOCR register) 
-		GPIO_PORTE_CR_R					=		0x1F;							// _ _ _ 1  1 1 1 1	(Change Commit registers)
-		GPIO_PORTE_AMSEL_R			=		0x00;							// _ _ _ 0  0 0 0 0	(Not using analog mode select)
-		GPIO_PORTE_PCTL_R				=		0x00000000;				// _ _ _ 0  0 0 0 0 (Not using alternative functions)
+		SYSCTL_RCGC2_R					|=	0x00000010;				// _ _ F E  D C B A (Port selection for clock)
+		delay 									= 	SYSCTL_RCGC2_R;		// set delay to wait for RCGC2 before continuing
+	
+		GPIO_PORTE_AMSEL_R			=		0x00;							// 0 0 0 0  0 0 0 0	(Not using analog mode select)
+		GPIO_PORTE_PCTL_R				=		0x00000000;				// 0 0 0 0  0 0 0 0 (Not using alternative functions)
 		GPIO_PORTE_DIR_R 			 &= 	~0x01;         		// PE0 (Input)
 		GPIO_PORTE_DIR_R 			 |= 	0x02;          		// PE1 (output)
 		GPIO_PORTE_AFSEL_R			=		0x00;							// 0 0 0 0  0 0 0 0 (Not using alternative functions)
 		GPIO_PORTE_PUR_R				=		0x00;							// 0 0 0 1  0 0 0 1 (Enable pull-up resistors on P0 and P4 switches)
 		GPIO_PORTE_DEN_R				=		0x1F; 						// 0 0 0 1  1 1 1 1	(Enable digital signal for all target pins)
 	
-	return 0;
 }
