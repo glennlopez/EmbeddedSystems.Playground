778bd851b406164551b65a7601ea459b3e630c9a
Glenn Lopez <glennlopez@gmail.com>
2017-02-13 16:29:09 -0600

    update

--- 01 - Independent Study/UTAustinX_610/Labware/Lab11_UART/UART_2.C
+++ 01 - Independent Study/UTAustinX_610/Labware/Lab11_UART/UART_2.C
@@ -1,4 +1,26 @@
-// Lab 11
+// UART.c
+// Runs on TM4C123 or LM4F120
+// Lab 11 involves switching this from UART1 to UART0.
+//                 switching from PC5,PC4 to PA1,PA0
+// Daniel Valvano
+// August 3, 2013
+
+/* This example accompanies the book
+   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
+
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
 // this connection occurs in the USB debugging cable
 // U0Rx (PA0) connected to serial port on PC
 // U0Tx (PA1) connected to serial port on PC
@@ -6,23 +28,22 @@
 
 #include "tm4c123gh6pm.h"
 #include "UART.h"
-#include <string.h>	//added this
+#include <string.h>
+
 
 // Function Prototypes
-unsigned char parseDigit(unsigned long n, int place, int leadingZero);	//added this
+unsigned char parseDigit(unsigned long n, int place, int leadingZero);
 
+	
 //------------UART_Init------------
 // Initialize the UART for 115200 baud rate (assuming 80 MHz UART clock),
 // 8 bit word length, no parity bits, one stop bit, FIFOs enabled
 // Input: none
 // Output: none
 void UART_Init(void){
-// [x]as part of Lab 11, modify this program to use UART0 instead of UART1
 //                 switching from PC5,PC4 to PA1,PA0
-	
   SYSCTL_RCGC1_R |= SYSCTL_RCGC1_UART0; // activate UART0
   SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOA; // activate port A
-	
   UART0_CTL_R &= ~UART_CTL_UARTEN;      // disable UART
   UART0_IBRD_R = 43;                    // IBRD = int(80,000,000 / (16 * 115200)) = int(43.402778)
   UART0_FBRD_R = 26;                    // FBRD = round(0.402778 * 64) = 26
@@ -73,6 +94,8 @@ void UART_OutChar(unsigned char data){
 //     valid range is 0 to 4294967295 (2^32-1)
 // Input: none
 // Output: 32-bit unsigned number
+// If you enter a number above 4294967295, it will return an incorrect value
+// Backspace will remove last digit typed
 unsigned long UART_InUDec(void){
 unsigned long number=0, length=0;
 char character;
@@ -96,51 +119,34 @@ char character;
   }
   return number;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/****************
- *	P A R T:  A	*
- ***************/
 //------------UART_OutString------------
 // Output String (NULL termination)
 // Input: pointer to a NULL-terminated string to be transferred
 // Output: none
 void UART_OutString(unsigned char buffer[]){
-	
-// [x]as part of Lab 11 implement this function
+// as part of Lab 11 implement this function
  while(*buffer){
     UART_OutChar(*buffer);
     buffer++;
   }
- 
 }
 
-
-/****************
- *	P A R T:  B	*
- ***************/
 unsigned char String[10];
 //-----------------------UART_ConvertUDec-----------------------
 // Converts a 32-bit number in unsigned decimal format
 // Input: 32-bit number to be transferred
 // Output: store the conversion in global variable String[10]
+// Fixed format 4 digits, one space after, null termination
+// Examples
+//    4 to "   4 "  
+//   31 to "  31 " 
+//  102 to " 102 " 
+// 2210 to "2210 "
+//10000 to "**** "  any value larger than 9999 converted to "**** "
 void UART_ConvertUDec(unsigned long n){
-	
-// [x]as part of Lab 11 implement this function
-if (n >= 10000) {
+	// as part of Lab 11 implement this function
+
+	if (n >= 10000) {
 		strcpy((char *) String, "****");
 	}
 	else {
@@ -151,44 +157,38 @@ if (n >= 10000) {
 	}
 	String[4] = 0x20;                // add a space at the end
 	String[5] = 0;                   // null termination
-  
-}	//function below is part of part b
+}
+
 unsigned char parseDigit(unsigned long n, int place, int leadingZero){	
 	int numHasStarted = place < n;
 	unsigned char digit = n % (place*10) / place;             
 	return digit + 0x20 + ((( digit > 0) || numHasStarted || leadingZero || place==1) * 0x10); 
 }
 
-
-
-
-
-
 //-----------------------UART_OutUDec-----------------------
 // Output a 32-bit number in unsigned decimal format
 // Input: 32-bit number to be transferred
 // Output: none
+// Fixed format 4 digits, one space after, null termination
 void UART_OutUDec(unsigned long n){
   UART_ConvertUDec(n);     // convert using your function
   UART_OutString(String);  // output using your function
 }
 
-
-
-
-
-
-/****************
- *	P A R T:  C	*
- ***************/
 //-----------------------UART_ConvertDistance-----------------------
 // Converts a 32-bit distance into an ASCII string
 // Input: 32-bit number to be converted (resolution 0.001cm)
 // Output: store the conversion in global variable String[10]
+// Fixed format 1 digit, point, 3 digits, space, units, null termination
+// Examples
+//    4 to "0.004 cm"  
+//   31 to "0.031 cm" 
+//  102 to "0.102 cm" 
+// 2210 to "2.210 cm"
+//10000 to "*.*** cm"  any value larger than 9999 converted to "*.*** cm"
 void UART_ConvertDistance(unsigned long n){
-	
-// []as part of Lab 11 implement this function
-  if (n >= 10000) {
+// as part of Lab 11 implement this function
+		if (n >= 10000) {
 		strcpy((char *) String, "*.***");
 	}
 	else {
@@ -200,16 +200,13 @@ void UART_ConvertDistance(unsigned long n){
 	}
 	strcat((char *) String, " cm");
 }
-
-
-
-
-
-
+  
 
 //-----------------------UART_OutDistance-----------------------
 // Output a 32-bit number in unsigned decimal fixed-point format
 // Input: 32-bit number to be transferred (resolution 0.001cm)
+// Output: none
+// Fixed format 1 digit, point, 3 digits, space, units, null termination
 void UART_OutDistance(unsigned long n){
   UART_ConvertDistance(n);      // convert using your function
   UART_OutString(String);       // output using your function
--- 01 - Independent Study/UTAustinX_610/Labware/Lab11_UART/uart.d
+++ 01 - Independent Study/UTAustinX_610/Labware/Lab11_UART/uart.d
@@ -1,3 +1,4 @@
 .\uart.o: UART.c
 .\uart.o: ..\tm4c123gh6pm.h
 .\uart.o: UART.h
+.\uart.o: D:\EngineeringSoftware\Keil\ARM\ARMCC\bin\..\include\string.h
