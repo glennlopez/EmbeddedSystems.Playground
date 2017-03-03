9a291e85914d9ee7a95a493c207a9759c46b30f6
Glenn Lopez <glennlopez@gmail.com>
2017-03-03 13:41:37 -0600

    added assets

--- 01 - Independent Study/UTAustinX_610/TExaSware/C12_EdgeInterrupt/EdgeInterrupt.c
+++ 01 - Independent Study/UTAustinX_610/TExaSware/C12_EdgeInterrupt/EdgeInterrupt.c
@@ -31,48 +31,51 @@
 
 #define NVIC_EN0_R              (*((volatile unsigned long *)0xE000E100))  // IRQ 0 to 31 Set Enable Register
 #define NVIC_PRI7_R             (*((volatile unsigned long *)0xE000E41C))  // IRQ 28 to 31 Priority Register
+
 #define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_IS_R         (*((volatile unsigned long *)0x40025404))
-#define GPIO_PORTF_IBE_R        (*((volatile unsigned long *)0x40025408))
-#define GPIO_PORTF_IEV_R        (*((volatile unsigned long *)0x4002540C))
-#define GPIO_PORTF_IM_R         (*((volatile unsigned long *)0x40025410))
-#define GPIO_PORTF_RIS_R        (*((volatile unsigned long *)0x40025414))
-#define GPIO_PORTF_ICR_R        (*((volatile unsigned long *)0x4002541C))
 #define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
 #define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
 #define GPIO_PORTF_PDR_R        (*((volatile unsigned long *)0x40025514))
 #define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
 #define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
 #define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+	
+#define GPIO_PORTF_IS_R         (*((volatile unsigned long *)0x40025404))
+#define GPIO_PORTF_IBE_R        (*((volatile unsigned long *)0x40025408))
+#define GPIO_PORTF_IEV_R        (*((volatile unsigned long *)0x4002540C))
+#define GPIO_PORTF_IM_R         (*((volatile unsigned long *)0x40025410))
+#define GPIO_PORTF_RIS_R        (*((volatile unsigned long *)0x40025414))
+#define GPIO_PORTF_ICR_R        (*((volatile unsigned long *)0x4002541C))
+	
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-#define SYSCTL_RCGC2_GPIOF      0x00000020  // port F Clock Gating Control
+//#define SYSCTL_RCGC2_GPIOF      0x00000020  // port F Clock Gating Control
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
-long StartCritical (void);    // previous I bit, disable interrupts
-void EndCritical(long sr);    // restore I bit to previous value
+//long StartCritical (void);    // previous I bit, disable interrupts
+//void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
 
 // global variable visible in Watch window of debugger
 // increments at least once per button press
 volatile unsigned long FallingEdges = 0;
 void EdgeCounter_Init(void){                          
-  SYSCTL_RCGC2_R |= 0x00000020; // (a) activate clock for port F
-  FallingEdges = 0;             // (b) initialize counter
-  GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
-  GPIO_PORTF_AFSEL_R &= ~0x10;  //     disable alt funct on PF4
-  GPIO_PORTF_DEN_R |= 0x10;     //     enable digital I/O on PF4   
-  GPIO_PORTF_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
-  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
-  GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
-  GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
-  GPIO_PORTF_IBE_R &= ~0x10;    //     PF4 is not both edges
-  GPIO_PORTF_IEV_R &= ~0x10;    //     PF4 falling edge event
-  GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
-  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4
-  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
-  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
-  EnableInterrupts();           // (i) Clears the I bit
+  SYSCTL_RCGC2_R 			|= 	0x00000020; 	// (a) activate clock for port F
+  FallingEdges 				 = 	0;            // (b) initialize counter
+  GPIO_PORTF_DIR_R 		&= 	~0x10;    		// (c) make PF4 in (built-in button)
+  GPIO_PORTF_AFSEL_R 	&= 	~0x10;  			//     disable alt funct on PF4
+  GPIO_PORTF_DEN_R 		|= 	0x10;     		//     enable digital I/O on PF4   
+  GPIO_PORTF_PCTL_R 	&= 	~0x000F0000; 	// configure PF4 as GPIO
+  GPIO_PORTF_AMSEL_R 	 = 	0;       			//     disable analog functionality on PF
+  GPIO_PORTF_PUR_R 		|= 	0x10;     		//     enable weak pull-up on PF4
+  GPIO_PORTF_IS_R 		&= 	~0x10;     		// (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R 		&= 	~0x10;    		//     PF4 is not both edges
+  GPIO_PORTF_IEV_R 		&= 	~0x10;    		//     PF4 falling edge event
+  GPIO_PORTF_ICR_R 		 = 	0x10;      		// (e) clear flag4
+  GPIO_PORTF_IM_R 		|= 	0x10;      		// (f) arm interrupt on PF4
+  NVIC_PRI7_R 				 = 	(NVIC_PRI7_R&0xFF00FFFF)|0x00A00000; // (g) priority 5
+  NVIC_EN0_R 					 = 	0x40000000;   // (h) enable interrupt 30 in NVIC
+  EnableInterrupts();           				// (i) Clears the I bit
 }
 void GPIOPortF_Handler(void){
   GPIO_PORTF_ICR_R = 0x10;      // acknowledge flag4
