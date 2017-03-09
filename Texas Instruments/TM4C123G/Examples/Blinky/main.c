d1c29fb562d75a71b5875268abc15974a260677f
Glenn Lopez <glennlopez@gmail.com>
2017-03-09 02:20:38 -0600

    update

--- Texas Instruments/TM4C123G/Examples/Blinky/main.c
+++ Texas Instruments/TM4C123G/Examples/Blinky/main.c
@@ -1,28 +1,89 @@
 /************************
  * ADDRESS DEFINITIONS
  ************************/
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))           //0x3FC means 0011 1111 expressed as 4*2^b (bitspecific addressing)
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))           //gpio direction            - offset 0x400
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))           //gpio alt function         - offset 0x420
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))           //gpio pull-up select       - offset 0x510
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))           //gpio digital enable       - offset 0x51C
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))           //gpio analog mode select   - offset 0x528
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))           //gpio port control         - offset 0x52C
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))           //gpio portf lock register  - offset 0x520
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))           //gpio portf comit register - offset 0x524
 
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))           //gpio clock gating (RCGC2) -   offset 0x108
-#define SYSCTL_RCGC2_GPIOF      0x00000020                                          //Port F Clock Gating Control | _ _ _ F E D C B A (ports)
+// System Control Legacy base address: 0x400F.E000 (Datasheet pg. 234)
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
 
+// PortF(APB) base address: 0x40025000 (Datasheet pg. 657)
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+
+// PortF Bit-specific Address: (7|200, 6|100, 5|80, 4|40, 3|20, 2|10, 1|08, 0|04)
+#define LED_B                   (*((volatile unsigned long *)0x40025010))
+#define LED_R                   (*((volatile unsigned long *)0x40025008))
+#define LED_G                   (*((volatile unsigned long *)0x40025020))
+
+#define ON                      0xFF
+#define OFF                     0x00
+
+// Prototypes
+void initPortF(void);
+void delay(unsigned int param);
+void blink_b(unsigned int param);
+void blink_g(unsigned int param);
 
 /************************
  * MAIN ROUTINE
  ************************/
-int main(void) {
-	
-	return 0;
+void main(void) {
+    initPortF();                     // Initialize PortF
+
+    while(1){
+        GPIO_PORTF_DATA_R ^= 0x02;
+        delay(100);
+    }
+
 }
 
 /************************
  * SUB ROUTINES
  ************************/
+
+// Initialize PortF as Digital
+void initPortF(void){    unsigned long volatile delay;
+
+    // Port Clock Control
+    SYSCTL_RCGC2_R          |=      0x00000020;
+    delay  /*Wait Ready*/    =      SYSCTL_RCGC2_R;
+
+    // GPIO Digital Control
+    GPIO_PORTF_DEN_R        |=      0x0E;
+    GPIO_PORTF_DIR_R        |=      0x0E;
+
+    // GPIO Alternate function control
+    GPIO_PORTF_AMSEL_R      &=      ~0x0E;
+    GPIO_PORTF_AFSEL_R      &=      ~0x0E;
+    GPIO_PORTF_PCTL_R       &=      ~0x0000FFF0;
+}
+
+// Busy-wait delay (~1ms per param)
+void delay(unsigned int param){ unsigned int i, j;
+
+    for(j = 0; j < param; j++){
+        for(i = 0; i < 800; i++){
+            // do nothing
+        }
+    }
+
+}
+
+// Blink Blue LED
+void blink_b(unsigned int param){
+    while(1){
+          GPIO_PORTF_DATA_R ^= 0x04;   // Toggle LED on/off
+          delay(param);                  // Up/Down time
+      }
+}
+
+//Blink Green LED
+void blink_g(unsigned int param){
+    while(1){
+        LED_G ^= ON;
+        delay(param);
+    }
+}
