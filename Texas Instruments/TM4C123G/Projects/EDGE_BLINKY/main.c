d5762f179fbb5ecd8a1f62d205c19f2d7f062f8a
Glenn Lopez <glennlopez@gmail.com>
2017-03-10 05:48:32 -0600

    adding comments

--- Texas Instruments/TM4C123G/Projects/EDGE_BLINKY/main.c
+++ Texas Instruments/TM4C123G/Projects/EDGE_BLINKY/main.c
@@ -14,11 +14,12 @@
 #define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
 
 // PortF Interrupt Registers
+#define NVIC_EN0_R              (*((volatile unsigned long *)0xE000E100))
+#define NVIC_PRI7_R             (*((volatile unsigned long *)0xE000E41C))
 #define GPIO_PORTF_IS_R         (*((volatile unsigned long *)0x40025404))
 #define GPIO_PORTF_IBE_R        (*((volatile unsigned long *)0x40025408))
 #define GPIO_PORTF_IEV_R        (*((volatile unsigned long *)0x4002540C))
 #define GPIO_PORTF_IM_R         (*((volatile unsigned long *)0x40025410))
-#define GPIO_PORTF_RIS_R        (*((volatile unsigned long *)0x40025414))
 #define GPIO_PORTF_ICR_R        (*((volatile unsigned long *)0x4002541C))
 
 // PortF GPIO Lock
@@ -40,17 +41,16 @@ void GPIOPortF_Handler(void){
 /************************
  * MAIN ROUTINE
  ************************/
-
-// Prototypes
 void initPortF_in(void);
 void initPortF_out(void);
 void delay(unsigned int param);
 void initNVIC(void);
 void EnableInterrupts(void);
+void WaitForInterrupt(void);
 
 void main(void) {
     // Initialization routine
-    SYSCTL_RCGC2_R |= 0x00000020;
+    SYSCTL_RCGC2_R |= 0x00000020;                   // (a) Activate PortF Clock
     initPortF_in();
     initPortF_out();
     initNVIC();
@@ -62,6 +62,7 @@ void main(void) {
             GPIO_PORTF_DATA_R ^= 0x08;
             delay(200);
         }
+        WaitForInterrupt();
     }
 }
 
@@ -73,43 +74,46 @@ void main(void) {
 // PortF Output Initialization
 void initPortF_out(void){
 
-    // GPIO Digital Control
-    GPIO_PORTF_DEN_R        |=      0x0E;
-    GPIO_PORTF_DIR_R        |=      0x0E;
+    // GPIO Digital Control                         //     ************OUTPUT*************
+    GPIO_PORTF_DEN_R        |=      0x0E;           // (b) Make PF1,2,3 Digital Pins
+    GPIO_PORTF_DIR_R        |=      0x0E;           //     Make PF1,2,3 OUTPUT Pins
 
     // GPIO Alternate function control
-    GPIO_PORTF_AMSEL_R      &=      0;
-    GPIO_PORTF_AFSEL_R      &=      ~0x0E;
-    GPIO_PORTF_PCTL_R       &=      ~0x0000FFF0;
+    GPIO_PORTF_AMSEL_R      &=      0;              //     Disable Analog Mode
+    GPIO_PORTF_AFSEL_R      &=      ~0x0E;          //     Disable Alternate Function on PF1,2,3
+    GPIO_PORTF_PCTL_R       &=      ~0x0000FFF0;    //     Keep PF1,2,3 as GPIO
 }
 
 // PortF Input Initialization
 void initPortF_in(void){
 
-    // UNLOCK PF0
-    GPIO_PORTF_LOCK_R        =      0x4C4F434B;
-    GPIO_PORTF_CR_R         |=      0x01;
+    // UNLOCK PF0                                   //     ************INPUT*************
+    GPIO_PORTF_LOCK_R        =      0x4C4F434B;     //     Unlock PF0
+    GPIO_PORTF_CR_R         |=      0x01;           //     Commit LOCK_R changes
 
     // GPIO Digital Control
-    GPIO_PORTF_DEN_R        |=      0x11;
-    GPIO_PORTF_DIR_R        &=      ~0x11;
-    GPIO_PORTF_PUR_R        |=       0x11;
+    GPIO_PORTF_DEN_R        |=       0x11;          // (b) Make PF4 & PF0 Digital Pins
+    GPIO_PORTF_DIR_R        &=      ~0x11;          //     Make PF4 & PF0 INPUT Pins
+    GPIO_PORTF_PUR_R        |=       0x11;          //     Set a PULL-UP resister internal for PF4 & PF1
 
     // GPIO Alternate function control
-    GPIO_PORTF_AMSEL_R       =      0;
-    GPIO_PORTF_AFSEL_R      &=      ~0x11;
-    GPIO_PORTF_PCTL_R       &=      ~0x000F000F;
+    GPIO_PORTF_AMSEL_R       =      0;              //     Disable Analog Mode
+    GPIO_PORTF_AFSEL_R      &=      ~0x11;          //     Disable Alternate Function on PF
+    GPIO_PORTF_PCTL_R       &=      ~0x000F000F;    //     Keep PF4 & PF0 as GPIO
 }
 
 // Edge Triggered Interrupt (pg. 657)
 void initNVIC(void){
+                                                    //     ************INTERRUPT*************
+    GPIO_PORTF_IS_R         &=      ~0x10;          // (c) PF4 edge-sensitive
+    GPIO_PORTF_IBE_R        &=      ~0x10;          //     PF4 does not trigger on both
+    GPIO_PORTF_IEV_R        &=      ~0x10;          //     PF4 triggers on falling edge
+    GPIO_PORTF_ICR_R        &=      ~0x00;          // (d) Clear flag 4 for PF4
+    GPIO_PORTF_IM_R         |=       0x10;          // (e) arm interrupt mask on PF4
 
-    GPIO_PORTF_IS_R         &=      ~0x10;
-    GPIO_PORTF_IBE_R        &=      ~0x10;
-    GPIO_PORTF_IEV_R        &=      ~0x10;
-    //GPIO_PORTF_IM_R         &=      ~0x00;
-    //GPIO_PORTF_RIS_R        &=      ~0x00;
-    //GPIO_PORTF_ICR_R        &=      ~0x00;
+    //NVIC Configuration
+    NVIC_PRI7_R            =        ~0x00;          // (f) Set Vector Priority to 5
+    NVIC_EN0_R             =        ~0X00;          // (g) Enable Global Interrupts
 
 }
 
